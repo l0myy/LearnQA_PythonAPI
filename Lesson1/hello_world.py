80685170e8de38ed046eda9cb9c51b026cfd1fae
l0myy <50821422+l0myy@users.noreply.github.com>
2021-06-23 22:53:25 +0300

    Added Lesson 2 and the first JSON parsing after GET request.

--- Lesson1/hello_world.py
+++ Lesson1/hello_world.py
@@ -1,4 +1,7 @@
 import requests
+import json
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
+payload = {"name": "User"}
+response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
 print(response.text)
+
