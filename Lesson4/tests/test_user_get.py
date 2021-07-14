a696496bae34e545b00fb1c1a5c7a77fc18f9e4e
l0myy <50821422+l0myy@users.noreply.github.com>
2021-07-14 21:52:55 +0300

    Homework 2. Autotest with get details from user using auth token another user.

--- Lesson4/tests/test_user_get.py
+++ Lesson4/tests/test_user_get.py
@@ -1,9 +1,13 @@
 from Lesson3.lib.base_case import BaseCase
 from Lesson3.lib.assertions import Assertions
 from Lesson3.lib.my_requests import MyRequests
+import allure
 
 
+@allure.epic("Testing methods to get user data.")
 class TestUserGet(BaseCase):
+
+    @allure.description("Testing methods to get details not auth users.")
     def test_get_user_details_not_auth(self):
         response = MyRequests.get("/user/2")
         Assertions.assert_json_has_key(response, 'username')
@@ -11,6 +15,7 @@ class TestUserGet(BaseCase):
         Assertions.assert_json_has_not_key(response, 'firstName')
         Assertions.assert_json_has_not_key(response, 'lastName')
 
+    @allure.description("Testing methods to get details auth user.")
     def test_get_user_details_auth_as_same_user(self):
         data = {
             'email': 'vinkotov@example.com',
@@ -30,3 +35,24 @@ class TestUserGet(BaseCase):
         expected_fields = ["username", "email", "firstName", "lastName"]
 
         Assertions.assert_json_has_keys(response2, expected_fields)
+
+    @allure.description("Testing methods to get details user with token another auth user.")
+    def test_get_user_details_auth_as_another_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        response1 = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookies(response1, 'auth_sid')
+        token = self.get_headers(response1, "x-csrf-token")
+
+        response2 = MyRequests.get(f"/user/2215",
+                                   headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid}
+                                   )
+
+        Assertions.assert_json_has_key(response2, 'username')
+        Assertions.assert_json_has_not_key(response2, 'email')
+        Assertions.assert_json_has_not_key(response2, 'firstName')
+        Assertions.assert_json_has_not_key(response2, 'lastName')
